7aa4f9efc1362bc285cd76bde08f65f722dc1e1f
hamza-133 <hamzabouelwahab04@gmail.com>
2026-01-12 15:56:45 +0100

    feat : add max 10 clicks limit and alert message , and add style .

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,16 @@ let colorBox = document.querySelector("#colorBox")
 let changeColorBtn = document.querySelector("#changeColorBtn")
 let clickCounter = document.querySelector("#clickCounter")
 
+
 let colors =   ["red", "blue", "green", "yellow", "purple",
     "cornflowerblue",
     "deeppink","chocolate" ,"chartreuse"]
     
     let lastcolor = 0
     let counter = 0
-    changeColorBtn.addEventListener("click" , () =>{
+    function handleclick() {
     let rendomIndex = Math.floor(Math.random() * colors.length) 
     console.log(rendomIndex);
-    
-    
     
     while(rendomIndex == lastcolor){
         rendomIndex = Math.floor(Math.random() * colors.length) 
@@ -20,5 +19,15 @@ let colors =   ["red", "blue", "green", "yellow", "purple",
     lastcolor = rendomIndex
 
     colorBox.style.backgroundColor = colors[rendomIndex]
+    counter++
+    clickCounter.textContent = "Clicks : " +  counter
     
-})
+    if (counter > 9) {
+        let alertMEssage = document.createElement("p")
+        alertMEssage.textContent = "You have reached the maximum of 10 clicks ."
+        alertMEssage.classList.add("alertMessage")
+        document.body.appendChild(alertMEssage)
+        changeColorBtn.removeEventListener("click" , handleclick)
+    }
+    }
+    changeColorBtn.addEventListener("click" , handleclick)
